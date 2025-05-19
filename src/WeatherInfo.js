9f69d51a3869c37888b6e5a77fa9c13ed1132d16
aebailu <anna.baillie@outlook.com>
2025-05-19 12:42:56 +0100

    Added functions for cleanup
    Cleaned up main app file and added search function

---
+++ src/WeatherInfo.js
@@ -0,0 +1,31 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="weatherInfo">
+      {" "}
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <img src={props.data.iconUrl} alt={props.data.description} />
+          <span className="temp">{Math.round(props.data.temperature)}</span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul class="sub-li">
+            <li>Feels like: {Math.round(props.data.feelsLike)}°C</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {Math.round(props.data.wind)} mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
